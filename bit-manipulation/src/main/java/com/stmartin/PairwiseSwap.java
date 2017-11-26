00ee4b4a68bd568d6c6ef3206422f72cb2197538
Shen Tong <stmartinlh@163.com>
2017-11-26 16:52:28 +0800

    create pairwiseSwap

---
+++ bit-manipulation/src/main/java/com/stmartin/PairwiseSwap.java
@@ -0,0 +1,17 @@
+package com.stmartin;
+
+/**
+ * Created by shentong on 2017/11/23.
+ * Pairwise Swap: Write a program to swap odd and even bits in an integer with as few instructions as
+ possible (e . g. , bit 0 and bit 1 are swapped, bit 2 and bit 3 are swapped, and so on).
+ Hints : #145, #248, #328, #355
+ */
+class PairwiseSwap {
+    static int pairwiseSwap(int input) {
+        throw new RuntimeException();
+    }
+
+    static private void swapTwoBits(int input, int index1, int index2) {
+
+    }
+}
---
+++ bit-manipulation/src/test/java/com/stmartin/PairwiseSwapSpec.java
@@ -0,0 +1,31 @@
+package com.stmartin;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static com.stmartin.PairwiseSwap.*;
+
+/**
+ * Created by shentong on 2017/11/23.
+ * Spec:
+ * 1. input is an integer
+ * 2. output is an integer formed by swapping the odd and even bits of the input, such as swapping 0 bit and 1 bit,
+ * swapping 2 bit and 3 bit.
+ * 3. need to use as few instructions as possible.
+ */
+public class PairwiseSwapSpec {
+    private int input;
+    private int output;
+
+    @Before
+    public void setUp() {
+        input = 21;
+        output = 10;
+    }
+
+    @Test
+    public void swapPairwiseTest() {
+        assertEquals(output, pairwiseSwap(input));
+    }
+}
